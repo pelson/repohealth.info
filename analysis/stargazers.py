76b1559cbdf15801031b1a0b726a274097aa85a5
Phil Elson <pelson.pub@gmail.com>
2017-01-13 18:35:03 +1100

    Fixed the issue where there may be no stargazers.

--- analysis/stargazers.py
+++ analysis/stargazers.py
@@ -4,13 +4,14 @@ import plotly.graph_objs as go
 
 def stargazers_prep(payload):
     stargazers = pd.DataFrame.from_dict(payload['github']['stargazers'])
-    stargazers['starred_at'] = pd.to_datetime(stargazers['starred_at'])
+     if len(stargazers) == 0:
+        stargazers = {'starred_at': [], 'login': []}
+    else:
+        stargazers['starred_at'] = pd.to_datetime(stargazers['starred_at'])
     return stargazers
 
 
 def stargazers_viz(stargazers):
-    if len(stargazers) == 0:
-        stargazers = {'starred_at': [], 'login': []}
     stars = go.Scatter(
         x=stargazers['starred_at'],
         y=[i + 1 for i in range(len(stargazers['starred_at']))],
